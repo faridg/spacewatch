0462073aa5adcb0b8ba9efd80e99871563f22abc
faridg <farid.gassanov@gmail.com>
2018-02-14 23:12:09 -0500

    ...

--- spacewatch.ino
+++ spacewatch.ino
@@ -1,29 +1,40 @@
-//The code:
+#include <TinyGPS.h>
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int readkey;
-void setup() {
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+
+TinyGPS gps;
+void getgps(TinyGPS &gps);
+void setup()
+{
+  Serial.begin(4800);
   lcd.begin(16, 2);
-  lcd.print("Hello world");
 }
-void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print("foo");
-  readkey=analogRead(0);
-  if (readkey<50) {
+void getgps(TinyGPS &gps)
+
+{
+  float latitude, longitude;
+  
+  gps.f_get_position(&latitude, &longitude);
+  lcd.setCursor(0,0);
+  lcd.print("Lat:");
+  lcd.print(latitude,5);
+  lcd.print(" ");
+  lcd.setCursor(0,1);
+  lcd.print("Long:");
+  lcd.print(longitude,5);
+  lcd.print(" ");
+  delay(3000); 
   lcd.clear();
-  lcd.print("Button Right");
-  }
-  else if(readkey<195) {
-    lcd.clear();
-    lcd.print("Button Up");
-  }
-  else if(readkey<380) {
-    lcd.clear();
-    lcd.print("Button Down");
-  }
-  else if(readkey<790) {
-    lcd.clear();
-    lcd.print("Button Left");
+}
+void loop()
+{
+  byte a;
+  if ( Serial.available() > 0 ) 
+  {
+    a = Serial.read(); 
+    if(gps.encode(a)) 
+    {
+      getgps(gps); 
+    }
   }
 }
